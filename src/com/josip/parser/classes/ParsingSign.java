98548876b1c0a2bf8b14b801044f0a0308dc84d1
Josip Bošnjak <jbosnjak3@gmail.com>
2023-01-14 23:19:22 +0100

    close to make list work

--- src/com/josip/parser/classes/ParsingSign.java
+++ src/com/josip/parser/classes/ParsingSign.java
@@ -4,25 +4,68 @@ import java.util.Arrays;
 
 public class ParsingSign {
 public static final String[] values= {"AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz","0123456789"};
-public char[] listOFChars= {};
-public char[] temp= {};
+public char[] listOFChars;
 
 public char[] returnFieldOfChars() {
-    for (int i = 0; i < values.length; i++) {
-	  separateStrings(values[i]);
+
+		char[] vals=values[0].toCharArray();
+		char[] vals1=values[1].toCharArray();
+		
+		listOFChars = new char[vals.length+vals1.length];
+		int size_of_vals=vals.length;
+		int size_of_vals1=vals1.length;
+		
+		
+		
+	       int placeOfTheIndexInVals1=listOFChars.length-vals.length;
+        System.out.println("Difference "+placeOfTheIndexInVals1);
+		for(int index1=vals.length;index1<=listOFChars.length;index1++) {
+		
+  
+   
+			for(int k=0; k<vals1.length;k++) {
+		
+				 listOFChars[k]=vals1[k];
+				 
+		
+			
+			}
+
+		
+			setListOFChars(listOFChars);
+			placeOfTheIndexInVals1=size_of_vals1;
+			listOFChars=getListOFChars();
+			System.out.println(listOFChars);
+			char[] tmp=listOFChars;
+			for(int index=vals1.length;index<=listOFChars.length;index++) {
+				
+				  
+				   
+				for(int p=0; p<vals.length;p++) {
+			          
+					 listOFChars[p]=vals[p];
+					 
+			
+				
+				}
+				setListOFChars(listOFChars);
+		
 	}
+		
+		
+		}
+
+		
+		
+	
+		
+		
+		
+	
+
 	return listOFChars;
 }
 
-public char[] separateStrings(String value) {
-    for (int i = 0; i < value.length(); i++) {
-	   
-	   temp[i]=value.charAt(i);
-	    System.out.println(temp[i]);
-	}
-    System.out.println(temp);
-	return temp;
-}
 
 
 public char[] getListOFChars() {
--- src/com/josip/tečaj/glavneklase/Main.java
+++ src/com/josip/tečaj/glavneklase/Main.java
@@ -6,9 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char[] array= {};
+
 ParsingSign parsingSign= new ParsingSign();
-parsingSign.returnFieldOfChars();
+System.out.println(parsingSign.returnFieldOfChars());
+
 
 	}
 
